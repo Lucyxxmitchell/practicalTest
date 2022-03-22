c587a482eba953933ad5ff9015713007ef38cd8f
Lucyxxmitchell <2646175m@student.gla.ac.uk>
2022-03-22 15:53:24 +0000

    checkpoint created superuser

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 # Create your models here.
+class Courses(models.Model):
+    title = models.CharField(max_length=128, unique=True)
+    startDate = models.DateField()
+    description = models.CharField(max_length=1000)
+    slug = models.SlugField(unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Courses, self).save(*args, **kwargs)
+
+    class Meta:
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.title
+
--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from courses.models import Courses
 
 # Create your views here.
+def index(request):
+    courses_list = Courses.objects
+    context_dict = {}
+    context_dict['courses'] = courses_list
+
+    return render(request, 'courses/index.html', context=context_dict)
